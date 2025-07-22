f6ad8acbb314b3f5afc9d91efc716c6db89a89e7
Tejaswini Sahuji <tejusahuji@gmail.com>
2025-07-22 11:00:55 +0530

    Add files via upload

---
+++ Assignment5.py
@@ -0,0 +1,26 @@
+#Create dictionary of student marks
+#Task 1
+student_marks = {
+    "Alice": 85,
+    "Bob": 78,
+    "Charlie": 92,
+    "Diana": 88,
+    "Ethan": 74
+}
+student_name = input("Enter student name : ")
+if student_name in student_marks:
+    print(f"{student_name} marks : {student_marks[student_name]}")
+else:
+    print("Student not found")
+
+#task2
+
+# Step 1: Create a list of numbers from 1 to 10
+number = list(range(1,11))
+# Step 2: Extract the first five elements
+first_five =(number[:5])
+# Step 3: Reverse the extracted elements
+reversed_five = first_five[::-1]
+# Step 4: Print both the extracted list and the reversed list
+print("First five elements :", first_five)
+print("Reversed five elements :", reversed_five)
