dbab390a2627c686924d1ad5d80e70113b20f12b
Tejaswini Sahuji <tejusahuji@gmail.com>
2025-08-21 10:02:57 +0530

    Update Assignment5.py

--- Assignment5.py
+++ Assignment5.py
@@ -7,7 +7,9 @@ student_marks = {
     "Diana": 88,
     "Ethan": 74
 }
+#take user input
 student_name = input("Enter student name : ")
+#condition if student name is matched with key of student_marks variable
 if student_name in student_marks:
     print(f"{student_name} marks : {student_marks[student_name]}")
 else:
@@ -23,4 +25,5 @@ first_five =(number[:5])
 reversed_five = first_five[::-1]
 # Step 4: Print both the extracted list and the reversed list
 print("First five elements :", first_five)
+
 print("Reversed five elements :", reversed_five)
